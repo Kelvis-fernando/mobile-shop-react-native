d8c6d4db7ca6bb2b5fc5a93f0f51758c9b2a3261
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-05-03 16:39:37 -0300

    first commit

---
+++ src/colors.ts
@@ -0,0 +1,17 @@
+const Colors = {
+  main: "#48b600",
+  white: "#ffffff",
+  black: "#000000",
+  lightBlack: "#bcbcbc",
+  orange: "#fece31",
+  red: "#FF0000",
+  gray: "#F4F4F4",
+  subGray: "#eefae6",
+  deepGray: "#e1f0d7",
+  deepestGray: "#c4c4c4",
+  blue: "#3b87f9",
+  underline: "#e5e5e5",
+  paypal: "#ffb730",
+};
+
+export default Colors;
