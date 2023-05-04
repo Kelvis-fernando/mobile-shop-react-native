29320fefe9ea95a67865677482957d301addce9f
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-05-04 15:00:08 -0300

    chore: config header

--- App.tsx
+++ App.tsx
@@ -2,15 +2,24 @@ import Home from "./src/screens/Home";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Details from "./src/screens/Details";
+import { Button } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+export default function App({ navigation }: any) {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={Details} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,11 +1,22 @@
 import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import Colors from "../colors";
 
 const Details = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Details</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.main,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default Details;
